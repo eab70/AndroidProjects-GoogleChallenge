46bf2fa604ffe622e440cda21cbf18475d6b2d0d
Lis B <eabacus@msn.com>
2018-02-14 14:00:43 -0500

    Add files via upload

---
+++ T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -0,0 +1,33 @@
+package com.example.android.background.sync;
+
+import android.content.Context;
+
+import com.example.android.background.utilities.PreferenceUtilities;
+
+// (1) Create a class called ReminderTasks
+public class ReminderTasks {
+
+//(2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+    public static String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+
+// (3) Create a private static void method called incrementWaterCount
+// (4) Add a Context called context to the argument list
+// (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+    private static void incrementWaterCount(Context context) {
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+
+// (6) Create a public static void method called executeTask
+// (7) Add a Context called context and String parameter called action to the parameter list
+// (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+    public static void executeTask(Context context, String action) {
+        if(ACTION_INCREMENT_WATER_COUNT.equals(action)) {
+            incrementWaterCount(context);
+        }
+    }
+
+}
+
+
+
+
